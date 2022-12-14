3efd774514986464a6fedb0eed4ebf0afe19c5e5
Luís Eduardo <luiseduardorrg@gmail.com>
2022-12-14 13:37:44 -0300

    estrutura
    mudanças na estrutura do projeto

---
+++ Scripts/script.js
@@ -0,0 +1,58 @@
+let vidBrunno = document.getElementById("vidBrunno");
+let playPause = document.getElementById("playPause-btn");
+
+// //mudar botão play/pause
+
+
+// video.addEventListener('playing', function(){
+//     playPause.src = "./Assets/icons/pause.png"
+//   })
+  
+//   video.addEventListener('pause', function(){
+//     playPause.src = "./Assets/icons/play.png"
+//   })
+  
+//   video.addEventListener('click', playP)
+
+// playPause.addEventListener('click', playP)
+
+// function playP(){
+//   if(video.paused) {
+//     video.play()
+//     playPause.src = "./Assets/icons/pause.png"
+//   } else {
+//     video.pause()
+//     playPause.src = "./Assets/icons/play.png"
+//   }
+// }
+
+function recarregar() {
+    vidBrunno.load();
+
+}
+
+function retroceder() {
+    vidBrunno.currentTime -= 15;
+}
+
+function play() {
+    vidBrunno.play();
+}
+
+function pause() {
+    vidBrunno.pause();
+}
+
+function avancar() {
+    vidBrunno.currentTime += 15;
+}
+
+function mutar() {
+    vidBrunno.muted = true;
+}
+
+function aumentarTela() {
+
+}
+
+
